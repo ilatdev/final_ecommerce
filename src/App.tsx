7730c3c3050787747956ffccc58be13d727000e3
Ian Latour <latourian@gmail.com>
2021-07-31 21:32:34 -0300

    move fetch api from home to app.js

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,27 @@ import {
   Route,
   Redirect
 } from 'react-router-dom'
+
 import Home from './Components/Home/Home'
 import CoreLayout from './Components/CoreLayout'
+import Detail from './Components/Detail'
+import { useEffect } from 'react'
+import { useAppDispatch } from './app/hooks'
+import { fetchProducts } from './features/products/productsSlice'
 
 function App() {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchProducts())
+  }, [dispatch])
+
   return (
     <Router>
       <CoreLayout>
         <Switch>
-          <Route path="/product_detail">
-            <h1>Product detail</h1>
+          <Route path={`/product_detail/:id`}>
+            <Detail />
           </Route>
           <Route path="/catalogue">
             <h1>Catalogue</h1>
